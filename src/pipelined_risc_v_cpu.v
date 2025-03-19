f311f63624426ce69b85a8c602409db29a3eeb03
Nithin574 <nithins038@gmail.com>
2025-03-19 15:29:14 +0530

    Update pipelined_risc_v_cpu.v

--- src/pipelined_risc_v_cpu.v
+++ src/pipelined_risc_v_cpu.v
@@ -61,13 +61,13 @@ module pipelined_risc_v_cpu #(parameter WIDTH = 32) (clk, rst, out);
   //top module output
   assign out = alu_result_wb_w;
 
-  program_counter #(.WIDTH(32)) program_counter_inst(.clk(clk),
+  program_counter #(.WIDTH(8)) program_counter_inst(.clk(clk),
     .rst(rst),
 	  .pc_scr(instruction_w[6:0]),
     .current_ins_add(current_ins_add_w)
   );
 
-	progarm_memory #(.DEPTH(256), .WIDTH(8), .ADD_WIDTH(8)) progarm_memory_inst(.clk(clk), .add(current_ins_add_w[7:0]),
+  progarm_memory #(.DEPTH(256), .WIDTH(32), .ADD_WIDTH(8)) progarm_memory_inst(.clk(clk), .add(current_ins_add_w[7:0]),
     .instruction(instruction_w)
   );
 
