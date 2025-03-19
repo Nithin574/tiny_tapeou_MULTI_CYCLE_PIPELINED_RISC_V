412a32dab0b48d26b65bd37a2d37b1020fec3d47
Nithin574 <nithins038@gmail.com>
2025-03-19 16:53:05 +0530

    Update alu.v

--- src/alu.v
+++ src/alu.v
@@ -1,8 +1,8 @@
 module alu #(parameter WIDTH = 32,OP_WIDTH = 3) (alu_op, op1, op2, out);
   input [OP_WIDTH - 1:0] alu_op;      // 8-bit opcode that determines the operation
-  input [WIDTH - 1:0] op1;            // First operand
-  input [WIDTH - 1:0] op2;            // Second operand
-  output reg [WIDTH - 1:0] out;       // Output result
+  input [14:0] op1;            // First operand
+  input [14:0] op2;            // Second operand
+  output reg [WIDTH - 1:0] out = {WIDTH{1'b0}};       // Output result
 
   always @(alu_op, op1, op2) begin
     case(alu_op)
@@ -11,7 +11,7 @@ module alu #(parameter WIDTH = 32,OP_WIDTH = 3) (alu_op, op1, op2, out);
       3'b011: out = op1 & op2;               //bitwise AND of op1 and op2
       3'b100: out = op1 | op2;               // Bitwise OR of op1 and op2
       3'b101: out = op1 ^ op2;               // Bitwise XOR of op1 and op2
-      default: out = 32'h0000_0000;          // Default case: if no opcode matches, set out to 0
+      default: out = {WIDTH{1'b0}};          // Default case: if no opcode matches, set out to 0
     endcase
   end
 
