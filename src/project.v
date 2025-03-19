b3cbefacfc72ef7f833e740068d421869c0130fa
Nithin574 <nithins038@gmail.com>
2025-03-19 12:59:59 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -24,6 +24,6 @@ module tt_um_pipelined_risc_v_cpu (
   // List all unused inputs to prevent warnings
     wire _unused = &{ena, clk, rst_n, ui_in, uio_in, 1'b0};
 
-    pipelined_risc_v_cpu #(.WIDTH(16)) inst(clk, rst_n, out);
+    pipelined_risc_v_cpu #(.WIDTH(32)) inst(clk, rst_n, out);
 
 endmodule
