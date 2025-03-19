a1b908b535a8b34a4890a99b1b6f57be9c10ea65
Nithin574 <nithins038@gmail.com>
2025-03-19 16:12:25 +0530

    Update program_counter.v

--- src/program_counter.v
+++ src/program_counter.v
@@ -18,7 +18,7 @@ module program_counter #(parameter WIDTH = 32)(clk, rst, pc_scr,current_ins_add)
 	 end
   end
 
-  assign next_ins_add = current_ins_add + 1'b1; //increments the pc value by one or used to compute the next instruction
+	assign next_ins_add = current_ins_add[6:0] + 1'b1; //increments the pc value by one or used to compute the next instruction
 
 endmodule
 
