26813bd35a87a61313d93aedfd8668b6eeb48d66
Nithin574 <nithins038@gmail.com>
2025-03-19 19:14:07 +0530

    Update mux_2_1.v

--- src/mux_2_1.v
+++ src/mux_2_1.v
@@ -1,4 +1,4 @@
-module mux_2_1 #(parameter WIDTH = 16) (i0, i1, sel, mux_out);  //this module implements the parametraized 2:1 mux
+module mux_2_1 #(parameter WIDTH = 15) (i0, i1, sel, mux_out);  //this module implements the parametraized 2:1 mux
   input sel;
   input [WIDTH-1:0] i0;
   input [WIDTH-1:0] i1;
