b4c79a8e437759b2346e629f4b8105f1a520d9a9
Nithin574 <nithins038@gmail.com>
2025-03-19 11:26:35 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,13 +15,15 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+    wire [31:0] out;
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign {uio_out,uo_out} = out[15:0];
+  assign uio_oe  = 8'hff;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, ui_in, 1'b0};
+
+    pipelined_risc_v_cpu #(.WIDTH(32)) inst(clk, rst_n, out);
 
 endmodule
