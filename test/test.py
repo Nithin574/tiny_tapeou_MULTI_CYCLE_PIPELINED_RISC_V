c0593bc76d4db28eefac2d501c1e01008ee5bb75
Nithin574 <nithins038@gmail.com>
2025-03-20 10:20:52 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -38,7 +38,7 @@ async def test_project(dut):
     #assert dut.uo_out.value == 8
     assert concatenated_value == 8
 
-    await ClockCycles(dut.clk, 2)
+    await ClockCycles(dut.clk, 1)
     concatenated_value = (dut.uio_out.value.integer << 8) | dut.uo_out.value.integer
     #assert dut.uo_out.value == 8
     assert concatenated_value == 2
