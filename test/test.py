7b53819b4a66108466df665b23de1ec765b642d9
Nithin574 <nithins038@gmail.com>
2025-03-20 10:15:56 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -38,5 +38,11 @@ async def test_project(dut):
     #assert dut.uo_out.value == 8
     assert concatenated_value == 8
 
+    await ClockCycles(dut.clk, 2)
+    concatenated_value = (dut.uio_out.value.integer << 8) | dut.uo_out.value.integer
+    #assert dut.uo_out.value == 8
+    assert concatenated_value == 2
+    
+
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
